ec8169205d428dea87f1d653b28c981f3875abda
hbisevac1 <hbisevac1@etf.unsa.ba>
2021-10-14 14:27:17 +0200

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,22 @@
+package ba.unsa.etf.rpr;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        int n;
+        System.out.println("Unesite prirodan broj n: ");
+        Scanner ulaz = new Scanner(System.in);
+        n = ulaz.nextInt();
+        for (int i = 1; i <= n; i++) {
+            int suma = 0;
+            int pom = i;
+            do {
+                suma += pom % 10;
+                pom /= 10;
+            } while (pom != 0);
+            if(i%suma==0) System.out.println(i);
+        }
+    }
+}
